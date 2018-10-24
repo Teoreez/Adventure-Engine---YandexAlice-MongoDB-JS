f7e0eafdde1f5379711701fde34eb91a218bb0cd
Ivan <44024879+Teoreez@users.noreply.github.com>
2018-10-25 03:31:25 +0600

    +dev options +errors

--- index.js
+++ index.js
@@ -14,7 +14,21 @@ const ContinueGame = new Scene(CONTINUE_GAME);
 const mongoose = require('mongoose');
 const UserData = require('./models/users.js');
 const GameData = require('./models/gamedata.js');
-mongoose.connect('mongodb://localhost/Adventure');
+const options = {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	autoIndex: true, // Don't build indexes
+	reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+	reconnectInterval: 500, // Reconnect every 500ms
+	poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+	bufferMaxEntries: 0,
+	connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+	socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+	family: 4 // Use IPv4, skip trying IPv6
+};
+mongoose.connect('mongodb://localhost/Adventure', options);
 
 //Приветсвтие
 alice.command('', ctx => {
