13762e7d77f14d8fd7df624140ec112c2ffd54ff
Ivan <44024879+Teoreez@users.noreply.github.com>
2018-10-25 16:17:19 +0600

    fixes fixes fixes

--- index.js
+++ index.js
@@ -12,32 +12,34 @@ const ContinueGame = new Scene(CONTINUE_GAME);
 
 //База данных
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Adventure');
 const UserData = require('./models/users.js');
 const GameData = require('./models/gamedata.js');
-mongoose.connect('mongodb://localhost/Adventure');
+
 
 //Приветсвтие
-alice.command('', ctx => {
+alice.command('', ctx => 
 	Reply.text('Добро пожаловать! Желаете начать игру или продолжить?', {
 		buttons: ['Начать игру', 'Продолжить', 'Об игре'],
-	  });
-})
-alice.command('Об игре', ctx =>{
-	Reply.text('Я даже и не знаю с чего начать...');
-})
+	  }),
+);
+alice.command('Об игре', ctx =>
+	Reply.text('Я даже и не знаю с чего начать...'),
+);
 //Начать
-alice.command('Продолжить игру' || 'Начать игру', ctx => { 
+alice.command(['Продолжить игру', 'Начать игру'], ctx => function (ctx) {
 	UserData.findOne({ uid: ctx.userId }).exec(function (err, userdata) {
 		if (err) return handleError(err);
 		return userdata();
-	})
+	});
 	if (userdata.uid != ctx.userId) {
-		Reply.text('Придумайте имя?'),
+		return Reply.text('Придумайте имя?'),
 		ctx.enter(NameSelect);
 	} else {
+		return Reply.text('Я тебя помню, продолжим...'),
 		ctx.enter(NewGame);
-	}
-})
+	};
+});
 //Переходим в стадию зщадачи имени ctx.message => в имя
 NameSelect.any(ctx => {
 	var NewName = new UserData({
@@ -48,7 +50,7 @@ NameSelect.any(ctx => {
 	ctx.state.stateofgame = userdata.stateofgame
 	ctx.enter(NEW_GAME);
 	Reply.text('Скажите что угодно что бы начать...');
-})
+});
 //Начало цепочки для нового игрока
 NewGame.any(ctx => {
 	GameData.findOne({ state: userdata.stateofgame}).exec(function (err, gamedata){
@@ -58,7 +60,7 @@ NewGame.any(ctx => {
 	
 	Reply.text(gamedata.text, { buttons: gamedata.buttons})
 	ctx.enter(NEXT_MOVE);
-})
+});
 //Определение варианта ответа и переход в продолжение цепочки
 NextMove.any(ctx => {	
 	var buttonid = gamedata.buttons.lastIndexOf(ctx.message);
@@ -72,7 +74,7 @@ NextMove.any(ctx => {
 		Reply.text('Вы не можете так поступить...');
 		ctx.enter(CONTINUE_GAME);
 	}
-})
+});
 //Продолжение диалога с возвращением к стадии разбора ответа от пользователя
 ContinueGame.any(ctx => {
 	GameData.findOne({ state: ctx.state.stateofgame}).exec(function (err,gamedata){
@@ -81,9 +83,9 @@ ContinueGame.any(ctx => {
 	})
 	Reply.text(gamedata.text, { buttons: gamedata.buttons})
 	ctx.enter(NEXT_MOVE);
-})
+});
 
 
 //Сервер
 const port = 3001
-alice.listen('/', port, callback => console.log(port))
+alice.listen(port, '/', callback => console.log(port))
