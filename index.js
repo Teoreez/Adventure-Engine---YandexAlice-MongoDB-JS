653d9089c844cd06da87d6c5977ad2ef1ab07624
Ivan <44024879+Teoreez@users.noreply.github.com>
2018-10-26 21:35:02 +0600

    just async everything

--- index.js
+++ index.js
@@ -46,13 +46,13 @@ alice.command('', ctx =>
 alice.command('Об игре', ctx =>
 	Reply.text('Я даже и не знаю с чего начать...'),
 );
-const startNewGame = ctx => {
+const startNewGame = async ctx => {
     ctx.session.set('stateofgame', '0');
     ctx.enter(NameSelect);
     return Reply.text('Придумайте новое имя');
 };
 
-const startGame = ctx => {
+const startGame = async ctx => {
 	const userdata = await queryname(ctx.userId);
 	const gamedata = await querygame(userdata.stateofgame);
 	ctx.session.set('stateofgame', userdata.stateofgame);
@@ -64,8 +64,8 @@ const startGame = ctx => {
 alice.command('Начать новую игру', startNewGame);
 alice.command('Продолжить', startGame);
 
-NameSelect.any( ctx => Reply.text(
-	function (ctx){
+NameSelect.any(async ctx => Reply.text(
+	async function (ctx){
 		const newname = ctx.message;
 		await createname(newname);
 		return 'Ваше имя' + ctx.message + 'верно?';
@@ -75,18 +75,18 @@ NameSelect.command('да', startGame);
 NameSelect.command('нет', startNewGame);
 
 
-NextMove.any(ctx => {	
+NextMove.any( async ctx => {	
 	var buttonid = gamedata.buttons.lastIndexOf(ctx.message);
 	if (ctx.message == gamedata.buttons[0] || gamedata.buttons[1] || gamedata.buttons[2] || gamedata.buttons[3] || gamedata.buttons[4]) {
 	ctx.state.set('stateofgame', gamedata.goto[buttonid]);
-	await update(ctx.state.stateofgame)
+	update(ctx.state.stateofgame)
 	ctx.enter(CONTINUE_GAME);
 	} else {
 		Reply.text('Вы не можете так поступить...');
 	}
 });
 
-ContinueGame.any(ctx => {
+ContinueGame.any( async ctx => {
 	const gamedata = await querygame(ctx.session.stateofgame);
 	Reply.text(gamedata.text, { buttons: gamedata.buttons})
 	ctx.enter(NEXT_MOVE);
